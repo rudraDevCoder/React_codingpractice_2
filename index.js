e658f6853c6e5806052904751670972145cf837f
rudra <rudra8997@gmail.com>
2023-01-29 11:58:54 +0000

    Ract_2

---
+++ index.js
@@ -0,0 +1,22 @@
+const element = (
+  <div>
+    <h1 className="congratulations">Congratulations</h1>
+    <div className="card-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="profile-heading">Kiran V</h1>
+
+      <p className="profile-para">
+        Vishnu institute of computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
